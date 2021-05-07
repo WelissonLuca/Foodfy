eb743bc22c2d99c00b6fa0d54526988dce90553a
Welisson luca <welissonluca17@gmail.com>
2021-05-07 01:51:41 -0300

    feat: toogle button show and hide description recipes

--- public/js/index.js
+++ public/js/index.js
@@ -8,4 +8,25 @@ function activeMenuItems() {
     }
   }
 }
+
+function hideShow() {
+  const hideShowButton = document.querySelectorAll('.button');
+  console.log(hideShowButton);
+  for (let i = 0; i < hideShowButton.length; i += 1) {
+    hideShowButton.item(i).addEventListener('click', () => {
+      const detailsContent = document.querySelectorAll('.details-content')[i];
+
+      if (hideShowButton.item(i).innerHTML == 'ESCONDER') {
+        hideShowButton.item(i).innerHTML = 'MOSTRAR';
+
+        detailsContent.style.display = 'none';
+      } else if (hideShowButton.item(i).innerHTML == 'MOSTRAR') {
+        hideShowButton.item(i).innerHTML = 'ESCONDER';
+
+        detailsContent.style.display = 'block';
+      }
+    });
+  }
+}
 activeMenuItems();
+hideShow();
